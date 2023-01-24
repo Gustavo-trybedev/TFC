dbad4e8963b3ab008bbcd85f869cc5c8e9cdf44b
gustavoBarros <gustavo.dutra11@gmail.com>
2023-01-24 12:10:46 -0300

    Users, Teams & Matches models

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,47 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    type: DataTypes.NUMBER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  homeTeamId: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  timestamps: false,
+  modelName: 'match',
+  underscored: true,
+});
+
+export default Match;
---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,27 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.NUMBER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  timestamps: false,
+  modelName: 'team',
+  underscored: true,
+});
+
+export default Team;
