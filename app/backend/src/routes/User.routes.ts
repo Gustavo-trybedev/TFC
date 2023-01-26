61dfd6b87d47f6152a877d67d3f62b52d8bd9a84
gustavoBarros <gustavo.dutra11@gmail.com>
2023-01-26 17:59:39 -0300

    feat: login router

---
+++ app/backend/src/routes/User.routes.ts
@@ -0,0 +1,24 @@
+import * as express from 'express';
+
+import LoginController from '../controllers/User.controller';
+import LoginValidation from '../middlewares/loginValidation';
+
+const loginValidationMiddleware = new LoginValidation();
+const loginController = new LoginController();
+
+const router = express.Router();
+
+router.post(
+  '/',
+  loginValidationMiddleware.validateUser,
+  loginValidationMiddleware.validateFields,
+  loginController.login,
+);
+
+router.get(
+  '/validate',
+  loginValidationMiddleware.validateToken,
+  loginController.userRole,
+);
+
+export default router;
