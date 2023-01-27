a30d3bfa189bbca4b5b914f53c6e2998541771cc
gustavoBarros <gustavo.dutra11@gmail.com>
2023-01-27 00:11:32 -0300

    feat: getById and underscore unused params

--- app/backend/src/controllers/Team.controller.ts
+++ app/backend/src/controllers/Team.controller.ts
@@ -1,18 +1,25 @@
-import { Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import TeamService from '../services/Team.service';
 
 const teamService = new TeamService();
 
 export default class TeamController {
   public getAllTeams = async (
+    _req: Request,
     res: Response,
-    next: NextFunction,
+    _next: NextFunction,
   ) => {
-    try {
-      const teams = await teamService.getAllTeams();
-      return res.status(200).json(teams);
-    } catch (error) {
-      next(error);
-    }
+    const teams = await teamService.getAllTeams();
+    return res.status(200).json(teams);
+  };
+
+  public getTeamById = async (
+    req: Request,
+    res: Response,
+    _next: NextFunction,
+  ) => {
+    const { id } = req.params;
+    const team = await teamService.getTeamById(Number(id));
+    return res.status(200).json(team);
   };
 }
