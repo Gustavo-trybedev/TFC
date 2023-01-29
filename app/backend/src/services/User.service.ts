76f6bbd2441d97456b8562ab498c0796b4bcb92a
gustavoBarros <gustavo.dutra11@gmail.com>
2023-01-29 20:23:08 -0300

    refactor: change interface

--- app/backend/src/services/User.service.ts
+++ app/backend/src/services/User.service.ts
@@ -1,10 +1,9 @@
-import { ILogin } from '../interfaces/ILogin';
-import User from '../database/models/Users';
+import { IUser } from '../interfaces/ILogin';
+import UserModel from '../database/models/Users';
 
 export default class LoginService {
-  public getUserByEmail = async (email: string): Promise<ILogin | undefined> => {
-    const user = await User.findOne({ where: { email } });
-    if (!user) return undefined;
-    return user;
+  public getUserByEmail = async (email: string): Promise<IUser> => {
+    const user = await UserModel.findOne({ where: { email } });
+    return user as IUser;
   };
 }
