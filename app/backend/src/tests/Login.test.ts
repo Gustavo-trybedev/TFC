bcd7f8a1246ba7b8b2139cf2285b1c5a764dd399
gustavoBarros <gustavo.dutra11@gmail.com>
2023-01-29 20:24:53 -0300

    feat: 80% coverage

---
+++ app/backend/src/tests/Login.test.ts
@@ -0,0 +1,108 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { App } from '../app';
+
+import { token, validAdmin, validLogin } from './mocks/loginMock';
+
+import UserModel from '../database/models/Users';
+
+import { Response } from 'superagent';
+
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const { app } = new App();
+
+let chaiHttpResponse: Response;
+
+describe('Login test', () => {
+  
+  afterEach(() => sinon.restore());
+
+
+  it('1 - Should return an error when try to login without email', async () => {
+    sinon.stub(UserModel, 'findOne').resolves(validAdmin as UserModel);
+    chaiHttpResponse = await chai
+    .request(app)
+    .post('/login')
+    .send({ password: validAdmin.password });
+
+    expect(chaiHttpResponse).to.have.status(400);
+    expect(chaiHttpResponse.body).to.have.property('message');
+    expect(chaiHttpResponse.body.message).to.be.deep.equal('All fields must be filled');
+  });
+
+
+  it('2 - Should return an error when try to login with invalid email', async () => {
+    sinon.stub(UserModel, 'findOne').resolves(validAdmin as UserModel);
+    chaiHttpResponse = await chai
+    .request(app)
+    .post('/login')
+    .send({ email: 'invalidEmail', password: validAdmin.password });
+
+    expect(chaiHttpResponse.status).to.be.equal(401);
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Incorrect email or password' });
+
+  });
+
+  it('3 - Should return an error when try to login without password', async () => {
+    sinon.stub(UserModel, 'findOne').resolves(validAdmin as UserModel);
+    chaiHttpResponse = await chai
+    .request(app)
+    .post('/login')
+    .send({ email: validAdmin.email });
+
+    expect(chaiHttpResponse.status).to.be.equal(400);
+    expect(chaiHttpResponse.body).to.have.property('message');
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'All fields must be filled'});
+  });
+
+  it('4 - Should return an error when try to login with invalid password', async () => {
+    sinon.stub(UserModel, 'findOne').resolves(validAdmin as UserModel);
+    chaiHttpResponse = await chai
+    .request(app)
+    .post('/login')
+    .send({ email: validAdmin.email, password: 'invalidPassword' });
+
+    expect(chaiHttpResponse.status).to.be.equal(401);
+    expect(chaiHttpResponse.body).to.have.property('message');
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Incorrect email or password' });
+  });
+
+  it('5 - Should return status 200 and a token in case of successful login', async () => {
+    sinon.stub(UserModel, 'findOne').resolves(validAdmin as UserModel);
+    sinon.stub(bcrypt, 'compareSync').returns(true);
+
+    chaiHttpResponse = await chai
+    .request(app)
+    .post('/login')
+    .send(validLogin);
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.have.property('token');
+  });
+
+  it('6 - Should return the user role', async () => {
+    sinon.stub(UserModel, 'findOne').resolves(validAdmin as UserModel);
+
+    chaiHttpResponse = await chai
+    .request(app)
+    .get('/login/validate')
+    .set('authorization', token);
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.have.property('role');
+    expect(chaiHttpResponse.body.role).to.be.deep.equal('admin');
+  });
+
+});
+
+
+
---
+++ app/backend/src/tests/mocks/loginMock.ts
@@ -0,0 +1,42 @@
+
+export const validAdmin = {
+  id: 1,
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: 'secret_admin',
+};
+
+export const invalidAdmin = {
+  id: 1,
+  username: 'Admin',
+  role: 'undefined',
+  email: 'admin@xablau.com',
+  password: 'senha_invalida',
+};
+
+export const validUser = {
+  id: 2,
+  username: 'User',
+  role: 'user',
+  email: 'user@user.com',
+  password: 'secret_user',
+};
+
+export const invalidUser = {
+  id: 2,
+  username: 'User',
+  role: 'undefined',
+  email: 'user@xablau.com',
+  password: 'senha_invalida',
+};
+
+export const validLogin = {
+  email: 'admin@admin.com',
+  password: 'secret_admin',
+};
+
+export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInVzZXIiOnsiaWQiOjEsInVzZXJuYW1lIjoiQWRtaW4iLCJyb2xlIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JHhpLkh4azFjekFPMG5aUi4uQjM5M3UxMGFFRDBSUTFOM1BBRVhRN0h4dExqS1BFWkJ1LlBXIn0sImlhdCI6MTY3NTAzMTYzMCwiZXhwIjoxNjc1NjM2NDMwfQ.B8AbDPjD5nhe7yVAvBjDhfpyouGtSkFd7uk7Ax6VM34"
+
+
+  
