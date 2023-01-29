669de8bbee2f09f448e6fe4f620c3f476acd01cd
gustavoBarros <gustavo.dutra11@gmail.com>
2023-01-29 20:21:10 -0300

    refactor: ILogin interface

--- app/backend/src/interfaces/ILogin.ts
+++ app/backend/src/interfaces/ILogin.ts
@@ -1,10 +1,10 @@
-export interface IUser {
+export interface IUser extends ILogin {
   id?: number;
   username: string;
   role: string;
 }
 
-export interface ILogin extends IUser {
+export interface ILogin {
   email: string;
   password: string;
 }
