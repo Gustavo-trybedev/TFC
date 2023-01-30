5cfe082e85c8f48e0f47130c715df9dc807a3aab
gustavoBarros <gustavo.dutra11@gmail.com>
2023-01-30 12:35:59 -0300

    feat: matches routes

--- app/backend/src/routes/Match.routes.ts
+++ app/backend/src/routes/Match.routes.ts
@@ -1,10 +1,19 @@
 import * as express from 'express';
 import MatchController from '../controllers/Match.controller';
+import MatchValidation from '../middlewares/matchValidation';
 
 const matchController = new MatchController();
+const matchValidation = new MatchValidation();
 
 const router = express.Router();
 
 router.get('/', matchController.getAllMatches);
+router.post(
+  '/',
+  matchValidation.validateTeamExistence,
+  matchValidation.validateTeams,
+  matchController.insertNewMatch,
+);
+router.patch('/:id/finish', matchController.updateMatch);
 
 export default router;
