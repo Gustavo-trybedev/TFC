c01076ce7f8ebedc0b2308f48625611804ab2b2b
gustavoBarros <gustavo.dutra11@gmail.com>
2023-01-30 12:35:29 -0300

    feat: insert and update match

--- app/backend/src/services/Match.service.ts
+++ app/backend/src/services/Match.service.ts
@@ -1,5 +1,5 @@
 import MatchesModel from '../database/models/Matches';
-import { IMatches } from '../interfaces/IMatch';
+import { IMatches, IMatchesBody } from '../interfaces/IMatch';
 import Team from '../database/models/Teams';
 
 export default class MatchesService {
@@ -19,5 +19,21 @@ export default class MatchesService {
       ],
     });
     return matches as unknown as IMatches[];
+  };
+
+  public insertNewMatch = async (match: IMatchesBody): Promise<IMatchesBody> => {
+    const newMatch = await MatchesModel.create({ ...match, inProgress: true });
+
+    return newMatch;
+  };
+
+  public updateMatch = async (id: number): Promise<IMatchesBody | undefined> => {
+    const match = await MatchesModel.findByPk(id);
+
+    if (!match) return undefined;
+
+    await match.update({ ...match, inProgress: false });
+
+    return match;
   };
 }
